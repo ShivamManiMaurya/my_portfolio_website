09249e2fbd8b75ae57460494fc5c5017e1588933
Shivam Maurya <109658264+ShivamManiMaurya@users.noreply.github.com>
2023-04-20 13:07:08 +0530

    initial commit

---
+++ src/components/NavBar.js
@@ -0,0 +1,51 @@
+import React from "react";
+import { Navbar, Container, Nav, Button } from "react-bootstrap";
+import logo from "./images/shivam_logo_transparentBackground.png";
+import "./styles/NavBar.css";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import GitHubIcon from "@mui/icons-material/GitHub";
+import InstagramIcon from "@mui/icons-material/Instagram";
+
+function NavBar() {
+    return (
+        <Navbar bg="dark" expand="lg" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home" className="">
+                    <a href="#home">
+                        <img className="logo" src={logo} alt="navbar-logo" />
+                    </a>
+                </Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#skills">Skills</Nav.Link>
+                        <Nav.Link href="#projects">Projects</Nav.Link>
+                        <Nav.Link href="#Education">Education</Nav.Link>
+                    </Nav>
+                    <Nav className="connections">
+                        <Nav className="social">
+                            <Nav.Link className="social-icon" href="#">
+                                <LinkedInIcon />
+                            </Nav.Link>
+                            <Nav.Link className="social-icon" href="#">
+                                <GitHubIcon />
+                            </Nav.Link>
+                            <Nav.Link className="social-icon" href="#">
+                                <InstagramIcon />
+                            </Nav.Link>
+                        </Nav>
+
+                        <Nav.Link href="#contact">
+                            <Button variant="outline-light">
+                                Let's Connect
+                            </Button>
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default NavBar;
